ed361a86aa40535f50bc5dd8201da526b926d003
jack-leyland <jackjleyland@gmail.com>
2022-11-30 17:44:16 -0500

    fix overflowing button

--- src/features/auth/components/AuthFormInputBox.tsx
+++ src/features/auth/components/AuthFormInputBox.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 import {
   View,
   TextInput,
@@ -6,14 +6,13 @@ import {
   ViewStyle,
   StyleSheet,
   TouchableOpacity,
-} from "react-native";
-import { AntDesign } from "@expo/vector-icons";
+} from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
 
-import { ThonburiLight } from "../../../common/components/StyledText";
-import Colors from "../../../common/constants/Colors";
-import useColorScheme from "../../../common/hooks/useColorScheme";
-import { FormStatus, PasswordFormStatus } from "../types";
-
+import { ThonburiLight } from '../../../common/components/StyledText';
+import Colors from '../../../common/constants/Colors';
+import useColorScheme from '../../../common/hooks/useColorScheme';
+import { FormStatus, PasswordFormStatus } from '../types';
 
 interface AuthFormTextInputBoxProps {
   topText: String;
@@ -24,22 +23,24 @@ interface AuthFormTextInputBoxProps {
   validator?: (value: string) => void;
 }
 
-// This component will validate on Blur of input field if a validator is provided in the props 
-
+// This component will validate on Blur of input field if a validator is provided in the props
 
 export default function AuthFormTextInputBox(props: AuthFormTextInputBoxProps) {
   const theme = useColorScheme();
-  const [value, setValue] = useState<string>("");
+  const [value, setValue] = useState<string>('');
   const [activeBoxStyle, setActiveBoxStyle] = useState<StyleProp<ViewStyle>>(
     styles.inputBox
   );
   const [textColor, setTextColor] = useState<string>(Colors[theme].text);
-  const [inputTextColor, setInputTextColor] = useState<string>(Colors.light.text);
+  const [inputTextColor, setInputTextColor] = useState<string>(
+    Colors.light.text
+  );
   const [hidePass, setHidePass] = useState<boolean>(props.isPassword);
-  const [status, setStatus] = useState<AuthFormTextInputBoxProps["status"]>(props.status)
-  
+  const [status, setStatus] = useState<AuthFormTextInputBoxProps['status']>(
+    props.status
+  );
+
   useEffect(() => {
-  
     if (props.status.isBadInput) {
       setActiveBoxStyle([styles.inputBox, styles.inputBoxBad]);
       setTextColor(Colors.common.red);
@@ -49,26 +50,37 @@ export default function AuthFormTextInputBox(props: AuthFormTextInputBoxProps) {
       setTextColor(Colors[theme].text);
       setInputTextColor(Colors.light.text);
     }
-    setStatus(props.status)
+    setStatus(props.status);
   }, [props.status]);
-
 
   return (
     <View style={styles.container}>
       <ThonburiLight style={[styles.topText, { color: textColor }]}>
         {props.topText}
       </ThonburiLight>
-      <View style={[activeBoxStyle, theme === "light" ? {shadowColor: "black", shadowOffset:{height:2, width:0}, shadowRadius:4, shadowOpacity: 0.1} : {}]}>
+      <View
+        style={[
+          activeBoxStyle,
+          theme === 'light'
+            ? {
+                shadowColor: 'black',
+                shadowOffset: { height: 2, width: 0 },
+                shadowRadius: 4,
+                shadowOpacity: 0.1,
+              }
+            : {},
+        ]}
+      >
         <TextInput
           onChangeText={(text) => {
             setValue(text);
-            props.onChange(text)
+            props.onChange(text);
           }}
           value={value}
           style={[styles.textInput, { color: inputTextColor }]}
           maxLength={50}
           keyboardAppearance={theme}
-          keyboardType={props.isEmail ? "email-address" : "default"}
+          keyboardType={props.isEmail ? 'email-address' : 'default'}
           returnKeyType="done"
           enablesReturnKeyAutomatically={true}
           secureTextEntry={hidePass}
@@ -81,16 +93,16 @@ export default function AuthFormTextInputBox(props: AuthFormTextInputBoxProps) {
             if (!status.isBadInput) {
               setActiveBoxStyle(styles.inputBox);
             }
-            props.validator?.(value)
+            props.validator?.(value);
           }}
         />
         {value && (
           <TouchableOpacity
             style={styles.closeButtonParent}
             onPress={() => {
-              setValue("")
-              props.onChange("")
-              props.validator?.("")
+              setValue('');
+              props.onChange('');
+              props.validator?.('');
             }}
           >
             <AntDesign
@@ -111,20 +123,24 @@ export default function AuthFormTextInputBox(props: AuthFormTextInputBoxProps) {
       </View>
       {status.isBadInput && !props.isPassword && (
         <View style={styles.badTextContainer}>
-          <ThonburiLight style={{ color: textColor, fontSize: 12}}>
-            {"badInputText" in status && status.badInputText}
+          <ThonburiLight style={{ color: textColor, fontSize: 12 }}>
+            {'badInputText' in status && status.badInputText}
           </ThonburiLight>
         </View>
       )}
       {status.isBadInput && props.isPassword && (
         <View style={styles.criteriaContainer}>
-          {"criteria" in status && status.criteria.map((text, index)=>{
-            return (
-              <ThonburiLight key={index} style={[styles.passwordCriteria,{color: textColor}]}>
-                {text}
-              </ThonburiLight>
-            )
-          })}
+          {'criteria' in status &&
+            status.criteria.map((text, index) => {
+              return (
+                <ThonburiLight
+                  key={index}
+                  style={[styles.passwordCriteria, { color: textColor }]}
+                >
+                  {text}
+                </ThonburiLight>
+              );
+            })}
         </View>
       )}
     </View>
@@ -133,27 +149,28 @@ export default function AuthFormTextInputBox(props: AuthFormTextInputBoxProps) {
 
 const styles = StyleSheet.create({
   container: {
-    width: "80%",
-    justifyContent: "center",
-    alignItems: "center",
+    width: '80%',
+    justifyContent: 'center',
+    alignItems: 'center',
     paddingBottom: 20,
   },
   textInput: {
-    height: "100%",
+    height: '100%',
     fontSize: 18,
     padding: 10,
-    paddingRight: 30,
-    justifyContent: "center",
-    alignItems: "center",
+    //paddingRight: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
     flexGrow: 1,
+    flex: 1,
   },
   topText: {
-    width: "100%",
+    width: '100%',
     marginLeft: 10,
     fontSize: 12,
   },
   inputBox: {
-    width: "100%",
+    width: '100%',
     marginTop: 5,
     marginBottom: 5,
     height: 50,
@@ -161,8 +178,8 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     borderWidth: 2,
     borderColor: Colors.light.background,
-    flexDirection: "row",
-    justifyContent: "space-between",
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
   inputBoxBad: {
     borderWidth: 2,
@@ -173,25 +190,26 @@ const styles = StyleSheet.create({
     borderColor: Colors.common.brightYellow,
   },
   badTextContainer: {
-    width: "100%",
+    width: '100%',
     height: 20,
-    position: "absolute",
+    position: 'absolute',
     bottom: 0,
-    alignItems: "center",
+    alignItems: 'center',
   },
   criteriaContainer: {
-    width: "100%",
+    width: '100%',
     height: 40,
   },
   closeButtonParent: {
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
     marginRight: 5,
+    width: '10%',
   },
   passwordCriteria: {
     fontSize: 12,
-    width:"100%",
-    textAlign: "left",
-    marginLeft: 10
-  }
+    width: '100%',
+    textAlign: 'left',
+    marginLeft: 10,
+  },
 });
